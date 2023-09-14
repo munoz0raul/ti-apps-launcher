3bea49e54fe16007ce0023e7be5b0855b82ae2fa
Raul Muñoz <raul@foundries.io>
2023-09-14 03:22:15 -0300

    Updating app

--- configs/am62xx-evm.cpp
+++ configs/am62xx-evm.cpp
@@ -68,7 +68,7 @@ ArmAnalytics arm_analytics;
 Benchmarks benchmarks;
 Gpu_performance gpuperformance;
 
-RunCmd *docker_app = new RunCmd(QStringLiteral("/usr/bin/pyqt5.sh"));
+RunCmd *docker_app = new RunCmd(QStringLiteral("/bin/bash /usr/bin/pyqt5.sh"));
 RunCmd *firefox_browser = new RunCmd(QStringLiteral("docker run -v /run/user/1000/:/tmp/ -i --env http_proxy --env https_proxy --env no_proxy --env XDG_RUNTIME_DIR=/tmp/ --env WAYLAND_DISPLAY=wayland-1 -u user ghcr.io/texasinstruments/seva-browser:v1.0.0 https://www.ti.com/microcontrollers-mcus-processors/arm-based-processors/overview.html"));
 RunCmd *demo_3d = new RunCmd(QStringLiteral("/usr/bin/SGX/demos/Wayland/OpenGLESSkinning"));
 
